a83eeb620d4f1a4053a1555114f6fc374a8a6f8f
Karu95 <karuan.osi@hotmail.de>
2021-04-30 08:44:44 +0200

    added questions

--- src/pr/Question.java
+++ src/pr/Question.java
@@ -5,13 +5,29 @@ public class Question {
     public static final String[] questions = 	{
             "Was ist int?",
             "Was ist Integer",
-            "Welcher dieser Datentypen ist kein primitiver Datentyp?"
+            "Welcher dieser Datentypen ist kein primitiver Datentyp?",
+            "Wie lautet der Rumpf der Methode, die beim Aufruf des Programms aufgerufen wird?",
+            "Durch welchen Operator l‰sst man eine Klasse von einer anderen (normalen Klasse) vererben?",
+            "Wie heiﬂt die Exception die auftritt, wenn man auf das 13. Element eines Arrays zugreift, welches nur eine L‰nge von 10 hat?",
+            "Wie erzeugt man in Java einen Mehrzeiligen Kommentar?",
+            "Wie erzeugt man in Java einen Einzeiligen Kommentar?",
+            "Bei welcher Antwort handelt es sich um Mathematische Funktionen?",
+            "Was ist eine Applikation in Java?",
+            "Welche 3 wichtige Eigenschaften hat ein Objekt?"
 
     };
     public static final String[][] options = 	{
             {"Utility Klasse","Wrapper Klasse","Primitiver Datentyp"},
             {"Klasse","Wrapper Klasse","Utility Klasse"},
-            {"int","double","String"}
+            {"int","double","String"},
+            {"puclic int main()","public static void start(String[] args)","public static void main(String() args)"},
+            {"Implements","Extends","inherits"},
+            {"ArrayIndexOutOfBoundsExeption","NullPointerExeption","ArrayLenghtExeption"},
+            {"/* Kommentar */","/# Kommentar #/","**/ Kommentar **/"},
+            {"///Kommentar","**/Kommenter","// Kommentara"},
+            {"Math.plus, Math.minus,Math.geteilt","Math.sin(x), Math.sqrt(x), Math.log(x)","funktion.max(x,y), funktion.min(x,y) funktion.cos(x)"},
+            {"eigenst‰ndiges Programm, das nur virtuelle Maschine als Interpreter braucht und Ñstand-aloneì lauff‰hig ist","Programm, das nur als Unterprogram eines Webbrowsers l‰uft","Eine Handy App"},
+            {"Charakter, Aussehen, Verhalten",""}
     };
     public static final char[] answers = 		{
             'A',
